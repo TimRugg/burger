4869f39838e9cd6ba918d4774b1c358f05bb4a51
timster2938 <ruggt001@icloud.com>
2018-02-25 18:51:41 -0500

    structure complete

--- config/orm.js
+++ config/orm.js
@@ -3,27 +3,36 @@ var connection = require("../config/connection.js");
 
 // Object for all our SQL statement functions.
 var orm = {
-    selectAll: function(cb) {
-      var queryString = "SELECT * FROM burgers";
-      connection.query(queryString, function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      });
-    },
-    insertOne: function(burger_name, devoured, cb) {
-        var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)";
-        connection.query(queryString, [burger_name, devoured], function(err, result) {
-          if (err) throw err;
-          console.log(result);
-        });        
-    },
-    updateOne: function(id, burger_name, devoured, cb) {
-        var queryString = "UPDATE burgers (burger_name, devoured) VALUES (?, ?) WHERE id = ?";
-        connection.query(queryString, [burger_name, devoured, id], function(err, result) {
-          if (err) throw err;
-          console.log(result);
-        });  
-    }
+  selectAll: function (cb) {
+    var queryString = "SELECT * FROM burgers;";
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+
+      console.log("selectAll");
+      console.log(result);
+
+    });
+  },
+  insertOne: function (burger_name, devoured, cb) {
+    var queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, ?);";
+    connection.query(queryString, [burger_name, devoured], function (err, result) {
+      if (err) throw err;
+
+      console.log("insertOne");
+      console.log(result);
+
+    });
+  },
+  updateOne: function (id, burger_name, devoured, cb) {
+    var queryString = "UPDATE burgers (burger_name, devoured) VALUES (?, ?) WHERE id = ?;";
+    connection.query(queryString, [burger_name, devoured, id], function (err, result) {
+      if (err) throw err;
+
+      console.log("updateOne");
+      console.log(result);
+
+    });
+  }
 };
 
 // Export the orm object for the model (cat.js).
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,43 +12,35 @@ router.get("/", function(req, res) {
       var hbsObject = {
         burgers: data
       };
+
+      console.log("router get");
       console.log(hbsObject);
+      
       res.render("index", hbsObject);
     });
   });
 
-//***********************************************************************
-// the following routes are still set up to send column names 
-// but the model file is set up with fixed columns
-// waiting to see if the handlebar files need dynamic columns
-//************************************************************************
 // burger.insertOne
-router.post("/api/burgers", function(req, res) {
-    burger.insertOne([
-      "name", "sleepy"
-    ], [
-      req.body.name, req.body.sleepy
-    ], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
+router.post("/", function(req, res) {
+
+    console.log("router post");
+    console.log(res);
+
+    burger.insertOne(req.body.burger_name, req.body.devoured, function() {
+      // refresh the html
+      res.redirect("/");
     });
   });
 
 // burger.updateOne
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
+router.put("/:id", function(req, res) {
   
-    console.log("condition", condition);
+    console.log("router put");
+    console.log(res);
   
-    burger.updateOne({
-      sleepy: req.body.sleepy
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+    burger.updateOne(req.body.id, req.body.burger_name, req.body.devoured, function() {
+        // refresh the html
+        res.redirect("/");
     });
   });
   
